f50e9be548b2bd754bf93c8c35e20a6560344671
Sung Won Cho <mikeswcho@gmail.com>
2015-10-01 11:17:51 +1000

    Write test for #exploreAt

--- test/explorer_test.js
+++ test/explorer_test.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect;
 var sinon = require('sinon');
+var robot = require('robotjs');
 
 var Explorer = require('../lib/explorer');
 
@@ -17,9 +18,23 @@ describe("Explorer", function(){
       var result = explorer.pixelsFound[0];
       expect(result.x).to.equal(300);
       expect(result.y).to.equal(125);
+    });
+  });
 
-      // Teardown
-      explorer._check.restore();
+  describe("#exploreAt", function(){
+    it("saves the pixels in pixelsFound", function(){
+      var explorer = new Explorer();
+
+      sinon.stub(robot, 'getPixelColor', function (x, y) {
+        if (x === 1 && y === 2) {
+          return 'ffffff';
+        }
+      });
+
+      explorer.exploreAt(1,2);
+
+      expect(explorer.pixelsFound[0].x).to.equal(1);
+      expect(explorer.pixelsFound[0].y).to.equal(2);
     });
   });
 });
